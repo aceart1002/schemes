f190a46143c6f23c8eab19c9341c2d907128eeac
aceart1002 <aceart1002@gmail.com>
2019-05-31 19:30:36 +0300

    merged with independent

--- src/api/java/aceart/api/ServerUpdater.java
+++
@@ -1,9 +0,0 @@
-package aceart.api;
-
-import net.minecraft.util.math.BlockPos;
-
-public interface ServerUpdater {
-	
-	void updateServer(BlockPos savePoint, String name, int rotationRender, BlockPos displacement, int operation);
-	
-}
